8fab17d9ddcb5c72a2036eb5c8b8a93b52d8e4be
yamen sahyouni <ys18@tu-clausthal.de>
2023-10-28 10:11:54 +0200

    create main App UI

---
+++ app/src/main/java/clock/gui/AppUI.java
@@ -0,0 +1,40 @@
+package clock.gui;
+
+import javax.swing.*;
+
+public class AppUI {
+    JFrame frame;
+    JTabbedPane tp;
+    JPanel clockP;
+    JPanel alarmP;
+    JPanel timerP;
+    JPanel stopwatchP;
+    ClockUI clockUI;
+
+    public AppUI() {
+        frame = new JFrame("Main");
+        tp = new JTabbedPane();
+        clockP = new JPanel();
+        alarmP = new JPanel();
+        timerP = new JPanel();
+        stopwatchP = new JPanel();
+        clockUI = new ClockUI();
+        
+        tp.add("Clock", clockUI);
+        tp.add("Alarm", alarmP);
+        tp.add("Timer", timerP);
+        tp.add("Stopwatch", stopwatchP);
+
+        frame.add(tp);
+
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new AppUI();
+    }
+
+}
