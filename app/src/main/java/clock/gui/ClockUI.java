82e5a5fe76afad0caec65acbb8167eaea73be77b
yamen sahyouni <ys18@tu-clausthal.de>
2023-10-29 09:46:51 +0100

    implement change time formats

--- app/src/main/java/clock/gui/ClockUI.java
+++ app/src/main/java/clock/gui/ClockUI.java
@@ -1,22 +1,31 @@
 package clock.gui;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.awt.event.*;
+import java.text.*;
+import java.util.*;
 import javax.swing.*;
 
 public class ClockUI extends JPanel implements Runnable {
     Thread thread = null;
     JButton button;
-    DateTimeFormatter dtf;
+    Calendar cal;
+    Date currentTime;
+    SimpleDateFormat formatter;
+    boolean is24Format = false; 
     
     
     public ClockUI() {
         this.setSize(600,400);
         thread = new Thread(this);
         thread.start();
-
+        
         button = new JButton();        
         button.setBounds(350,200,100,50);
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+                is24Format = is24Format ? false : true;
+            }
+        });
 
         this.add(button);
         this.setLayout(null);
@@ -26,22 +35,43 @@ public class ClockUI extends JPanel implements Runnable {
     public void run() {
         try {
             while (true) {
-                dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
-                LocalDateTime currentTime = LocalDateTime.now();
+
+                formatter = new SimpleDateFormat("hh:mm:ss");
+                cal = Calendar.getInstance();
+                currentTime = cal.getTime();
 
                 printTime(currentTime);
 
-                thread.sleep(1000);
+                Thread.sleep(1000);
             }
-            
-
         } catch (Exception e) {
             // TODO: handle exception
         }
     }
 
-    private void printTime(LocalDateTime currentTime) {
-        button.setText(dtf.format(currentTime));        
+    private void printTime(Date currentTime) {
+        String currentTimeString = formatter.format(currentTime);
+        int currentHourInt = Integer.parseInt(currentTimeString.substring(0,2));
+        String currentHour24FormatStr = "";
+        String am_pmString = "AM";
+        // we get 24 format
+        if(is24Format){
+            currentHour24FormatStr = 
+                String.valueOf(currentHourInt).concat((String) currentTimeString.subSequence(2, currentTimeString.length()));
+        } else {
+            if(currentHourInt>12){
+                currentHourInt -= 12;
+                am_pmString="PM";
+            } else if (currentHourInt<12){
+                if(currentHourInt == 0) currentHourInt +=12;
+                am_pmString="AM";
+            } else{
+                am_pmString="PM";
+            }
+            currentHour24FormatStr = 
+                String.valueOf(currentHourInt).concat((String) currentTimeString.subSequence(2, currentTimeString.length()) + " " + am_pmString);
+        }
+        button.setText(currentHour24FormatStr);        
     }
 
     public static void main(String[] args) {
